eab2bbcf5a956830951453497bbabaaadc2322ef
Kay Zheng <l04m33@gmail.com>
2012-11-09 11:26:13 +0800

    initial commit

---
+++ src/batnitor_app.erl
@@ -0,0 +1,16 @@
+-module(batnitor_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    batnitor_sup:start_link().
+
+stop(_State) ->
+    ok.
